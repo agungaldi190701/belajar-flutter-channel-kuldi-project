a80a0629ce67ef4a8ada8153d58bcc48a9351569
agungaldi190701 <agung20.mhs@akb.ac.id>
2023-02-19 15:35:51 +0700

    menggunakan ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner:
-//           false, // untuk menhilangkan tanda debug di pojok kanan atas
-//       home: Scaffold(
-//         //untuk bagian atas (header)
-
-//         appBar: AppBar(
-//           backgroundColor: Colors.black,
-//           title: const Text(
-//             "My Apps",
-//             style: TextStyle(
-//               fontFamily: "Better",
-//               fontSize: 40,
-//             ),
-//           ),
-//         ),
-
-//         //bagian body
-//         body: Center(
-//           // agar ke tengah
-//           child: const Text(
-//             "Selamat Datang",
-//             overflow: TextOverflow.ellipsis,
-//             textAlign: TextAlign.center,
-//             style: TextStyle(
-//               fontSize: 60,
-//               color: Color.fromARGB(255, 44, 44, 44),
-//               fontWeight: FontWeight.bold,
-//               //decoration: TextDecoration.underline,
-//               fontFamily: "Better",
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -60,30 +13,29 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Column'),
+          title: Text("List View"),
         ),
-        body: Row(
-          crossAxisAlignment: CrossAxisAlignment
-              .end, // merubah posisi kanan tengah kiri semua kolom
-          mainAxisAlignment: MainAxisAlignment
-              .center, // merubah posisi atas tengah bawah semua kolom
-          verticalDirection:
-              VerticalDirection.down, // merubah posisi atas  bawah Setiap kolom
+        body: ListView(
           children: [
             Container(
-              width: 50,
-              height: 100,
+              height: 400,
+              width: 400,
+              color: Colors.red,
+            ),
+            Container(
+              height: 400,
+              width: 400,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 400,
+              width: 400,
               color: Colors.green,
             ),
             Container(
-              width: 50,
-              height: 200,
+              height: 400,
+              width: 400,
               color: Colors.yellow,
-            ),
-            Container(
-              width: 50,
-              height: 300,
-              color: Colors.red,
             ),
           ],
         ),
