324af563eec1c52a7c7b4d49c4f6f4e5718376c5
agungaldi190701 <agung20.mhs@akb.ac.id>
2023-02-19 14:52:14 +0700

    Visible Dan Invisible Widget (Container)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,50 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner:
+//           false, // untuk menhilangkan tanda debug di pojok kanan atas
+//       home: Scaffold(
+//         //untuk bagian atas (header)
+
+//         appBar: AppBar(
+//           backgroundColor: Colors.black,
+//           title: const Text(
+//             "My Apps",
+//             style: TextStyle(
+//               fontFamily: "Better",
+//               fontSize: 40,
+//             ),
+//           ),
+//         ),
+
+//         //bagian body
+//         body: Center(
+//           // agar ke tengah
+//           child: const Text(
+//             "Selamat Datang",
+//             overflow: TextOverflow.ellipsis,
+//             textAlign: TextAlign.center,
+//             style: TextStyle(
+//               fontSize: 60,
+//               color: Color.fromARGB(255, 44, 44, 44),
+//               fontWeight: FontWeight.bold,
+//               //decoration: TextDecoration.underline,
+//               fontFamily: "Better",
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,34 +52,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:
-          false, // untuk menhilangkan tanda debug di pojok kanan atas
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        //untuk bagian atas (header)
-
         appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: const Text("My Apps"),
+          title: Text('Column'),
         ),
-
-        //bagian body
-        body: Center(
-          // agar ke tengah
-          child: const Text(
-            "Selamat Datang",
-            overflow: TextOverflow.ellipsis,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 60,
-              color: Color.fromARGB(255, 44, 44, 44),
-              fontWeight: FontWeight.bold,
-              //decoration: TextDecoration.underline,
-              fontFamily: "Better",
+        body: Stack(
+          
+          children: [
+            Container(
+              height: 400,
+              width: 400,
+              color: Colors.green,
             ),
-          ),
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.red,
+            ),
+          ],
         ),
       ),
     );
